b737ffc85da131e4dd8ed604a5db89115f6dd2f4
sky-aman <aman.dubey.sky@gmail.com>
2024-02-18 14:43:39 +0530

    project-done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable query={query} />
+      </Suspense>
+    </div>
+  );
 }
